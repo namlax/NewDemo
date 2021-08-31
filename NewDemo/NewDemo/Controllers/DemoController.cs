79f97001f3ec8a3bd624a798830c9a0659037af4
Lã Nam <namla2000k@gmail.com>
2021-08-31 09:39:07 +0700

    alien

---
+++ NewDemo/NewDemo/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult TinhTong()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult TinhTong(string soA, string soB)
+        {
+            double so1 = Convert.ToDouble(soA);
+            double so2 = Convert.ToDouble(soB);
+            return View();
+        }
+    }
+}
---
+++ NewDemo/NewDemo/Models/GPT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewDemo.Models
+{
+    public class GPT
+    {
+        public double TinhTong(double a, double b)
+        {
+            double tong = 0;
+            tong = a + b;
+            return tong;
+        }
+    }
+}
