d1e6ceeeecd6d2caf24d90db8deb807d59fd8c09
Lã Nam <namla2000k@gmail.com>
2021-08-31 10:14:50 +0700

    alien

--- NewDemo/NewDemo/Controllers/DemoController.cs
+++ NewDemo/NewDemo/Controllers/DemoController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using GiaiPhuongTrinh.Models;
-namespace GiaiPhuongTrinh.Controllers
+using NewDemo.Models;
+namespace NewDemo.Controllers
 {
     public class DemoController : Controller
     {
